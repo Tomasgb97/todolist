6bd6ac3fe86697d159165763571cebd30a856ae7
Tomasgb <tomasgomezbelcastro@gmail.com>
2021-05-12 19:30:50 -0300

    css modifications

--- src/domevents.js
+++ src/domevents.js
@@ -4,17 +4,24 @@ const domevent = (function (){
 const loginBttn = document.getElementById('loginbutton');
 const logoutBttn = document.getElementById('logoutbutton');
 
+const loggedin = document.querySelectorAll('.toremove');
+
+
 
 const login = () => {
 
     loginBttn.style.display = 'block';
     logoutBttn.style.display ='none';
+    loggedin.forEach(element => element.style.visibility = 'hidden');
+    
 };
 
 const logOut = () => {
 
     loginBttn.style.display = 'none';
     logoutBttn.style.display ='block';
+    loggedin.forEach(element => element.style.visibility ='visible');
+
     
 };
 
